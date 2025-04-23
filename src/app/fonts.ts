c9b75de14cc5913148c2a83dc2ec33d6f6716cdc
Zach Brown <zach@zb.dev>
2025-04-22 20:47:07 -0700

    adjust fonts

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,9 +1,27 @@
-import { Karma, Inter, Lato, Montserrat, Sorts_Mill_Goudy } from "next/font/google";
-import localFont from "next/font/local/"
+import {
+  Karma,
+  Inter,
+  Lato,
+  Montserrat,
+  Sorts_Mill_Goudy,
+  Red_Hat_Display,
+} from "next/font/google";
 
-
-export const karma = Karma ({subsets: ["latin"], weight: ["300","400","500","600","700"] })
+export const Rhd = Red_Hat_Display({
+  subsets: ["latin"],
+  weight: ["700"],
+});
+export const karma = Karma({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
 export const inter = Inter({ subsets: ["latin"] });
 export const lato = Lato({ subsets: ["latin"], weight: ["300", "700"] });
-export const monsterrat = Montserrat({subsets: ["latin"],weight: ["300", "700"],});
-export const SMG = Sorts_Mill_Goudy({ subsets: ['latin'], weight: ["400","400"]});
+export const monsterrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["300", "700"],
+});
+export const SMG = Sorts_Mill_Goudy({
+  subsets: ["latin"],
+  weight: ["400", "400"],
+});
