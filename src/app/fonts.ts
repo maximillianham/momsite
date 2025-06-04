f02257b0e026b7872fe0cb4cd727a414fbd12b10
Maximillian Hammer <maxjhammer@gmail.com>
2025-06-03 17:22:52 -0700

    added fonts

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,27 +1,11 @@
-import {
-  Karma,
-  Inter,
-  Lato,
-  Montserrat,
-  Sorts_Mill_Goudy,
-  Red_Hat_Display,
-} from "next/font/google";
+import { Karma, Inter, Lato, Montserrat, Sorts_Mill_Goudy, Red_Hat_Display, Playfair_Display, Cormorant_Garamond } from "next/font/google";
+import localFont from "next/font/local/"
 
-export const Rhd = Red_Hat_Display({
-  subsets: ["latin"],
-  weight: ["700"],
-});
-export const karma = Karma({
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700"],
-});
+export const CG = Cormorant_Garamond ({subsets: ["latin"], weight: [ "300", "400", "500", "600"]})
+export const Display = Playfair_Display({subsets: ["latin"], weight: [ "400", "500", "600", "700"]});
+export const Rhd = Red_Hat_Display({subsets: ["latin"], weight: ["300","400","500","600","700"] });
+export const karma = Karma ({subsets: ["latin"], weight: ["300","400","500","600","700"] });
 export const inter = Inter({ subsets: ["latin"] });
 export const lato = Lato({ subsets: ["latin"], weight: ["300", "700"] });
-export const monsterrat = Montserrat({
-  subsets: ["latin"],
-  weight: ["300", "700"],
-});
-export const SMG = Sorts_Mill_Goudy({
-  subsets: ["latin"],
-  weight: ["400", "400"],
-});
+export const monsterrat = Montserrat({subsets: ["latin"],weight: ["300", "700"],});
+export const SMG = Sorts_Mill_Goudy({ subsets: ['latin'], weight: ["400","400"]});
