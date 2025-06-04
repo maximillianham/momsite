e95c98469832fa3ca956fb1edf703b1706c48f9c
Maximillian Hammer <maxjhammer@gmail.com>
2025-06-03 17:23:12 -0700

    adjusted terms page

--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -1,14 +1,20 @@
+import { CG } from "@/app/fonts";
+
 export default function Terms() {
   return (
-    <main className="">
-      <h1>Terms & Conditions</h1>
-      <p>Effective Date: 6/2/2025</p>
+    <main
+      className={`bg-black text-white text-lg p-16 space-y-4 ${CG.className}`}
+    >
+      <h1 className={`text-center text-2xl font-semibold`}>
+        Terms & Conditions
+      </h1>
+      <p className="font-semibold">Effective Date: 6/2/2025</p>
       <p>
         Welcome to Straight to the Root. By accessing or using this website
         and/or engaging in any coaching services, you agree to be bound by the
         following Terms & Conditions. Please read them carefully.
       </p>
-      <h2>Overview</h2>
+      <h2 className="font-semibold">Overview</h2>
       <p>
         These Terms & Conditions (&quot;Terms&quot;) govern your use of the
         website [your domain] and all services provided by Straight to the Root
@@ -17,7 +23,7 @@ export default function Terms() {
         these Terms. If you do not agree, please do not use this website or our
         services.
       </p>
-      <h2>Services Disclaimer</h2>
+      <h2 className="font-semibold">Services Disclaimer</h2>
       <p>
         Coaching is a professional relationship designed to support your
         personal growth, clarity, and well-being. It is not a substitute for
@@ -29,13 +35,13 @@ export default function Terms() {
         We do not gurantee any specific results from coaching sessions. Your
         progress depends on your own efforts commitment, and follow-through.
       </p>
-      <h2>Eligibility</h2>b
+      <h2 className="font-semibold">Eligibility</h2>
       <p>
         You must be atleast 18 years of age to use this site or participate in
-        coaching services unless written consent is provided b y a parent or
-        legal guatrdian.
+        coaching services unless written consent is provided by a parent or
+        legal guardian.
       </p>
-      <h2>Payments, Refund & Cancelations</h2>
+      <h2 className="font-semibold">Payments, Refund & Cancelations</h2>
       <p>
         Payment for sessions is due in advance, unless otherwise agreed upon in
         writing. we accept Zelle, Venmo, Credit Card, PayPal, & Bank Transfer.
@@ -44,15 +50,22 @@ export default function Terms() {
         Refunds are not provided for complete sessions. Exceptions to this
         policy are at our sole discretion.
       </p>
-      <h2>Client Responsibilities</h2>
-      <p>You agree to&colon;</p>
-      <p>show up on time and prepared for your sessions.</p>
-      <p>be open, honest, and committed to your growth.</p>
-      <p>
-        understand that coaching is a collaborative processand results depend on
-        your active participation.
-      </p>
-      <h2>Confidentiality</h2>
+      <h2 className="font-semibold">Client Responsibilities</h2>
+      <p className="text-center">You agree to:</p>
+      <ul className="text-justify space-y-2 md:ml-32 md:w-1/2 ">
+        <li className="">
+          &#8226; Show up on time and prepared for your sessions.
+        </li>
+        <li className="">
+          {" "}
+          &#8226; Be open, honest, and committed to your growth.
+        </li>
+        <li className="">
+          &#8226; Understand that coaching is a collaborative process and
+          results depend on your active participation.
+        </li>
+      </ul>
+      <h2 className="font-semibold">Confidentiality</h2>
       <p>
         All information shared during sessions is treated with strict
         confidentiality, except where disclosure is required by law (e.g., risk
@@ -62,34 +75,34 @@ export default function Terms() {
         While we do our best to protect your information, please be aware that
         no method of internet transmission or electronic storage is 100% secure.
       </p>
-      <h2>Intellectual Property</h2>
+      <h2 className="font-semibold">Intellectual Property</h2>
       <p>
         All content on this website-- including text, logos, graphics,
         downloads, and materials-- is the intellectual property of Straight to
         the Root unless otherwise stated period. You may not use, reproduce, or
         distribute any materials without prior written permission.
       </p>
-      <h2>Limitation of Liability</h2>
+      <h2 className="font-semibold">Limitation of Liability</h2>
       <p>
         To the fullest extent permitted by law, we disclaim all liability for
         any damages arising from your use of this website or participation in
         coaching services, including indirect or consequential damages.
       </p>
-      <h2>Changes to These Terms</h2>
+      <h2 className="font-semibold">Changes to These Terms</h2>
       <p>
         We reserve the right to update or change these Terms at any time. Any
         updates will be posted on this page with a revised effective date.
         Continued use of the website or services after changes indicates your
         acceptance of the updated Terms.
       </p>
-      <h2>Governing Law</h2>
+      <h2 className="font-semibold">Governing Law</h2>
       <p>
         These Terms are governed by the laws of the State of California/USA
         without regard to its conflict of laws principles.
       </p>
-      <h2>Contact Us</h2>
-      <p>
-        If you have any questions about these Terms, please email us at&colon;
+      <h2 className="font-semibold">Contact Me:</h2>
+      <p className="text-center">
+        If you have any questions about these Terms, please email us at:
         ckurtz85@gmail.com
       </p>
     </main>
