336e035a45fb1c42b43fc7155def3383e4bc1150
Maximillian Hammer <maxjhammer@gmail.com>
2025-09-12 11:58:11 -0700

    Some work on a testimonial carousel and adjustments to the sizing and color of the sttr logo

---
+++ src/app/data/testimonials.tsx
@@ -0,0 +1,72 @@
+const testimonials = [
+  {
+    testimonial: `Carrie goes above and beyond, I believe she will excel in any role
+    that a client is facing. Carrie has displayed a strong work ethic,
+    high standards and a genuine care for the well being of clients. She
+    posseses above average emotional intelligence and excellent
+    communication skills. - Dr. Morgan Anderson Psy.D`,
+  },
+  {
+    testimonial: `Whatever you are trying to move through, you definitely want Carrie in
+    your corner. She will make you feel seen, heard and loved; while not
+    backing down on what you need to hear in order to move the needle.
+    Carries loving, nurturing approach will really help you feel safe,
+    looking at the uncomfortable stuff so that you can move through
+    anything that is holding you back. There is no staying stuck with
+    Carrie. She truly has a gift to help people and I feel so blessed to
+    get to work with her. It has been breakthrough after breakthrough and
+    my life is shifting in unbelievable ways because of her. - S, 55`,
+  },
+  {
+    testimonial: `Working with Carrie has been truly transformative. From our very first
+    session, her guidance and unwavering support has helped me gain
+    clarity and confidence in my personal and professional life. Her
+    unique approach to coaching, combined with her genuine empathy, has
+    empowered me to set and achieve meaningful goals. What I love most
+    about working with Carrie is how she consistently validates my
+    feelings and experiences, making me feel truly heard and understood.
+    I&apos;m constantly experiencing positive changes in my life daily,
+    with the way I act, feel, and view life; Thanks to Carrie! - M, 21`,
+  },
+  {
+    testimonial: `Carrie holds space like a mirror, calm and deeply present. In our
+    session, I felt completely safe to open up and drop into my body in a
+    way I hadn&apos;t before. What unfolded was pure magic, my fire and my
+    light finally stopped fighting and started dancing. The visual journey
+    she walked me through was freeing, and I left feeling more connected,
+    more embodied, and fully lit from within. Carrie helped me reclaim my
+    power, not by fixing, but by witnessing. She&apos;s a gift to the
+    nervous system, to the body and to the soul. - KM, Life Coach`,
+  },
+  {
+    testimonial: `Having Carrie as a supportive presence in my life has been invaluable
+    and I&apos;m beyond grateful our paths crossed. Carrie has a genuine
+    heart that you feel as she holds you gently through both a somatic and
+    emotional connection within. She has helped me be more gentle with
+    myself as I move through old wounds, sensations, and narratives where
+    I&apos;ve felt stuck. As a therapist/coach myself, I&apos;m rather
+    picky about who I can trust to have the skills and innate intuition to
+    guide me along my healing journey. She celebrates all the wins, big &
+    small and reflects back hard truths in the most loving, compassionate
+    way. Her knowledge and guidance through somatic practices has been a
+    game changer. Those of us that identify as empaths & space holders
+    need support too and I can&apos;t recommend Carrie enough to hold you
+    through a beautiful transformation of coming home to yourself. Working
+    with Carrie has felt like the most tender, energizing, and powerful
+    permission slip, giving me the confidence and graceful inner knowing
+    to feel safe, leading with my heart and trusting my body&apos;s
+    wisdom. - K 48, Therapist`,
+  },
+];
+
+export default function myFunc() {
+  return (
+    <div>
+      {testimonials.map((d) => (
+        <div key={d.testimonial}>
+          <p>{d.testimonial}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Roots from "./components/MomsiteLogoCav.png";
+import Roots from "./components/LotusColorSTTR.png";
 import Certificate from "./components/governingboard.png";
 import blossommain from "./components/blossommain.jpg";
 import blossomBG from "./components/adobestock_90133252.jpeg";
@@ -10,7 +10,70 @@ import Sunnyday from "./components/IMG_5889.jpeg";
 import sfembodiment from "./components/sf_highres.png";
 import { karma, Rhd } from "@/app/fonts";
 import AutoPlayVideo from "./components/AutoPlayVideo";
-import Terms from "./terms/page";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const testimonials = [
+  {
+    testimonial: `Carrie goes above and beyond, I believe she will excel in any role
+    that a client is facing. Carrie has displayed a strong work ethic,
+    high standards and a genuine care for the well being of clients. She
+    posseses above average emotional intelligence and excellent
+    communication skills. - Dr. Morgan Anderson Psy.D`,
+  },
+  {
+    testimonial: `Whatever you are trying to move through, you definitely want Carrie in
+    your corner. She will make you feel seen, heard and loved; while not
+    backing down on what you need to hear in order to move the needle.
+    Carries loving, nurturing approach will really help you feel safe,
+    looking at the uncomfortable stuff so that you can move through
+    anything that is holding you back. There is no staying stuck with
+    Carrie. She truly has a gift to help people and I feel so blessed to
+    get to work with her. It has been breakthrough after breakthrough and
+    my life is shifting in unbelievable ways because of her. - S, 55`,
+  },
+  {
+    testimonial: `Working with Carrie has been truly transformative. From our very first
+    session, her guidance and unwavering support has helped me gain
+    clarity and confidence in my personal and professional life. Her
+    unique approach to coaching, combined with her genuine empathy, has
+    empowered me to set and achieve meaningful goals. What I love most
+    about working with Carrie is how she consistently validates my
+    feelings and experiences, making me feel truly heard and understood.
+    I&apos;m constantly experiencing positive changes in my life daily,
+    with the way I act, feel, and view life; Thanks to Carrie! - M, 21`,
+  },
+  {
+    testimonial: `Carrie holds space like a mirror, calm and deeply present. In our
+    session, I felt completely safe to open up and drop into my body in a
+    way I hadn&apos;t before. What unfolded was pure magic, my fire and my
+    light finally stopped fighting and started dancing. The visual journey
+    she walked me through was freeing, and I left feeling more connected,
+    more embodied, and fully lit from within. Carrie helped me reclaim my
+    power, not by fixing, but by witnessing. She&apos;s a gift to the
+    nervous system, to the body and to the soul. - KM, Life Coach`,
+  },
+  {
+    testimonial: `Having Carrie as a supportive presence in my life has been invaluable
+    and I&apos;m beyond grateful our paths crossed. Carrie has a genuine
+    heart that you feel as she holds you gently through both a somatic and
+    emotional connection within. She has helped me be more gentle with
+    myself as I move through old wounds, sensations, and narratives where
+    I&apos;ve felt stuck. As a therapist/coach myself, I&apos;m rather
+    picky about who I can trust to have the skills and innate intuition to
+    guide me along my healing journey. She celebrates all the wins, big &
+    small and reflects back hard truths in the most loving, compassionate
+    way. Her knowledge and guidance through somatic practices has been a
+    game changer. Those of us that identify as empaths & space holders
+    need support too and I can&apos;t recommend Carrie enough to hold you
+    through a beautiful transformation of coming home to yourself. Working
+    with Carrie has felt like the most tender, energizing, and powerful
+    permission slip, giving me the confidence and graceful inner knowing
+    to feel safe, leading with my heart and trusting my body&apos;s
+    wisdom. - K 48, Therapist`,
+  },
+];
 
 export default function Home() {
   return (
@@ -27,7 +90,7 @@ export default function Home() {
 
         {/*LogoMark*/}
         <Image
-          className="relative md:left-1/4 z-10 pt-8"
+          className="relative md:left-1/3 px-4 z-10 pt-8"
           src={Roots}
           alt="logomark"
           height={150}
@@ -170,65 +233,8 @@ export default function Home() {
 
       <p className="text-center pt-50 text-3xl">Testimonials</p>
 
-      <div className="flex flex-col lg:flex-row text-justify lg:text-center lg:gap-x-12 pt-16 pb-16 shadow-md px-12 lg:px-16 gap-y-8">
-        <p className=" lg:w-1/5">
-          Carrie goes above and beyond, I believe she will excel in any role
-          that a client is facing. Carrie has displayed a strong work ethic,
-          high standards and a genuine care for the well being of clients. She
-          posseses above average emotional intelligence and excellent
-          communication skills. - Dr. Morgan Anderson Psy.D
-        </p>
-        <p className=" lg:w-1/5">
-          Whatever you are trying to move through, you definitely want Carrie in
-          your corner. She will make you feel seen, heard and loved; while not
-          backing down on what you need to hear in order to move the needle.
-          Carries loving, nurturing approach will really help you feel safe,
-          looking at the uncomfortable stuff so that you can move through
-          anything that is holding you back. There is no staying stuck with
-          Carrie. She truly has a gift to help people and I feel so blessed to
-          get to work with her. It has been breakthrough after breakthrough and
-          my life is shifting in unbelievable ways because of her. - S, 55
-        </p>
-        <p className=" lg:w-1/5">
-          Working with Carrie has been truly transformative. From our very first
-          session, her guidance and unwavering support has helped me gain
-          clarity and confidence in my personal and professional life. Her
-          unique approach to coaching, combined with her genuine empathy, has
-          empowered me to set and achieve meaningful goals. What I love most
-          about working with Carrie is how she consistently validates my
-          feelings and experiences, making me feel truly heard and understood.
-          I&apos;m constantly experiencing positive changes in my life daily,
-          with the way I act, feel, and view life; Thanks to Carrie! - M, 21
-        </p>
-        <p className=" lg:w-1/5">
-          Carrie holds space like a mirror, calm and deeply present. In our
-          session, I felt completely safe to open up and drop into my body in a
-          way I hadn&apos;t before. What unfolded was pure magic, my fire and my
-          light finally stopped fighting and started dancing. The visual journey
-          she walked me through was freeing, and I left feeling more connected,
-          more embodied, and fully lit from within. Carrie helped me reclaim my
-          power, not by fixing, but by witnessing. She&apos;s a gift to the
-          nervous system, to the body and to the soul. - KM, Life Coach
-        </p>
-        <p className="lg:w-1/5">
-          Having Carrie as a supportive presence in my life has been invaluable
-          and I&apos;m beyond grateful our paths crossed. Carrie has a genuine
-          heart that you feel as she holds you gently through both a somatic and
-          emotional connection within. She has helped me be more gentle with
-          myself as I move through old wounds, sensations, and narratives where
-          I&apos;ve felt stuck. As a therapist/coach myself, I&apos;m rather
-          picky about who I can trust to have the skills and innate intuition to
-          guide me along my healing journey. She celebrates all the wins, big &
-          small and reflects back hard truths in the most loving, compassionate
-          way. Her knowledge and guidance through somatic practices has been a
-          game changer. Those of us that identify as empaths & space holders
-          need support too and I can&apos;t recommend Carrie enough to hold you
-          through a beautiful transformation of coming home to yourself. Working
-          with Carrie has felt like the most tender, energizing, and powerful
-          permission slip, giving me the confidence and graceful inner knowing
-          to feel safe, leading with my heart and trusting my body&apos;s
-          wisdom. - K 48, Therapist
-        </p>
+      <div>
+        <div className="flex flex-col lg:flex-row text-justify lg:text-center lg:gap-x-8 pt-16 pb-16 shadow-md px-12 lg:gap-y-8"></div>
       </div>
       <p className="pt-36 pb-56 px-6 md:px-36 text-center tracking-widest text-3xl">
         &quot;At any given moment you have the power to say: this is not how the
